29966d09cbd7e3162cf67ed0bc60c0713c547bf4
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-02-25 23:39:09 +0400

    Using the search Function in the run Function

--- src/lib.rs
+++ src/lib.rs
@@ -19,7 +19,9 @@ impl Config {
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
-    println!("With text:\n{contents}");
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
 
     Ok(())
 }
@@ -42,9 +44,9 @@ mod tests {
     fn one_result() {
         let query = "duct";
         let contents = "\
-                Rust:
-                safe, fast, productive.
-                Pick three.";
+Rust:
+safe, fast, productive.
+Pick three.";
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
 }
